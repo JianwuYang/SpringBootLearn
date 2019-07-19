a7f9b582811e3a4205fe688269307937267aa054
JianwuYang <yangjianwu@hotmail.com>
2019-07-19 18:31:41 +0800

    MvcConfig @Override

--- src/main/java/com/viking/hello/config/MvcConfig.java
+++ src/main/java/com/viking/hello/config/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
         //定义了4个view controllers
